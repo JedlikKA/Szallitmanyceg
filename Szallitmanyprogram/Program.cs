cf25529896a82bfffbf2d606989013fc85f76d25
Körmöndi.Alex.István <Krmndi.Alex.Istvn@C102-05>
2020-02-21 09:02:43 +0100

    Az adatszerkezet elkészült.

---
+++ Szallitmanyprogram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szallitmanyprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] teljes = { "ABC-123", "ASD-234", "FGH-432", "BAB-986", "SEX-444" };
+            List<string> bent = new List<string>();
+            Console.ReadKey();
+        }
+    }
+}
