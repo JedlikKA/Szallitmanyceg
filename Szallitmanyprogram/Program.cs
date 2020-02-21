2182afae010445662c850931d2b963ae6e10feae
Körmöndi.Alex.István <Krmndi.Alex.Istvn@C102-05>
2020-02-21 09:11:05 +0100

    Kész a menű.

--- Szallitmanyprogram/Program.cs
+++ Szallitmanyprogram/Program.cs
@@ -12,6 +12,27 @@ namespace Szallitmanyprogram
         {
             string[] teljes = { "ABC-123", "ASD-234", "FGH-432", "BAB-986", "SEX-444" };
             List<string> bent = new List<string>();
+            string választás;
+            do
+            {
+                //A választási lehetőségek kiírása.
+                Console.WriteLine("Elmegy (E)");
+                Console.WriteLine("Bejön (B)");
+                Console.WriteLine("Kilépés (K)");
+                //A választás bekérése.
+                választás = Console.ReadLine();
+                switch (választás)
+                {
+                    case "E":Elmegy();
+                        break;
+                    case "B":Bejön();
+                        break;
+                    case "K":Kilépés();
+                        break;
+                    default: Console.WriteLine("Nem megfelelő a választás.");
+                        break;
+                }
+            } while (választás!="K");
             Console.ReadKey();
         }
     }
